c9a261d9c689f35317c172be272f1dde22656576
Huyen Nguyen <huyen.nguyen.0194@gmail.com>
2019-04-01 00:46:11 -0400

    updated

--- FA_pipeline_final.py
+++ FA_pipeline_final.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import subprocess
+import sys
 from utilities import functional_annotation
 from utilities import toGFF3
 from utilities import clustering
@@ -44,44 +45,59 @@ def main():
     
     if not outputFolder in os.listdir():
         subprocess.call(["mkdir",outputFolder])
-    if not door2_out.split("/")[1] in os.listdir(outputFolder): 
-        #functional_annotation.door2blast_local_all50(faa_dir,operonDB,"blast_operon_db",door2_out) 
-        functional_annotation.door2blast_server_all50(faa_dir,operonDB,"blast_operon_db",door2_out)
-    if not vfdb_out.split("/")[1] in os.listdir(outputFolder):
-        #functional_annotation.vfdbblast_local_all50(args.fna,vfdbDB,"blast_vfdb_db",vfdb_out)
-        functional_annotation.vfdbblast_server_all50(args.fna,vfdbDB,"blast_vfdb_db",vfdb_out)
-    if not card_out.split("/")[1] in os.listdir(outputFolder):
-        functional_annotation.rgi_all50(faa_dir,card_out)
-    if not tmhmm_out.split("/")[1] in os.listdir(outputFolder):      
-        functional_annotation.tmhmm_all50(faa_dir,tmhmm_out)
-    if not singalP_out.split("/")[1] in os.listdir(outputFolder):
-        functional_annotation.signalP_all50(faa_dir,singalP_out)
-    if not piler_out.split("/")[1] in os.listdir(outputFolder):      
-        functional_annotation.piler_all50(args.asem,piler_seq,piler_out)
-    if not eggNOG_out.split("/")[1] in os.listdir(outputFolder):
-        subprocess.call(["python2","utilities/eggnog.py",eggNOG_out])
-
+    try:
+        if not door2_out.split("/")[1] in os.listdir(outputFolder): 
+            #functional_annotation.door2blast_local_all50(faa_dir,operonDB,"blast_operon_db",door2_out) 
+            functional_annotation.door2blast_server_all50(faa_dir,operonDB,"blast_operon_db",door2_out)
+        if not vfdb_out.split("/")[1] in os.listdir(outputFolder):
+            #functional_annotation.vfdbblast_local_all50(args.fna,vfdbDB,"blast_vfdb_db",vfdb_out)
+            functional_annotation.vfdbblast_server_all50(args.fna,vfdbDB,"blast_vfdb_db",vfdb_out)
+        if not card_out.split("/")[1] in os.listdir(outputFolder):
+            functional_annotation.rgi_all50(faa_dir,card_out)
+        if not tmhmm_out.split("/")[1] in os.listdir(outputFolder):      
+            functional_annotation.tmhmm_all50(faa_dir,tmhmm_out)
+        if not singalP_out.split("/")[1] in os.listdir(outputFolder):
+            functional_annotation.signalP_all50(faa_dir,singalP_out)
+        if not piler_out.split("/")[1] in os.listdir(outputFolder):      
+            functional_annotation.piler_all50(args.asem,piler_seq,piler_out)
+        if not eggNOG_out.split("/")[1] in os.listdir(outputFolder):
+            subprocess.call(["python2","utilities/eggnog.py",eggNOG_out])
+    except Exception as e:
+        print(e)
+        raise SystemExit("please fix the error. Pipeline is terminated")
+       
 
     """ EGGNOG mapping: from one output of EGGNOG, map to 50 output files for 50 faa files"""
-    if not eggNOG_map_out.split("/")[1] in os.listdir(outputFolder):
-        names_file="allfiles.txt" #contains 50 id number of each genome
-        id_file_faa="title.txt" # contains header line in merge file that concatenate all 50 genomes faa/fna
-        mapping.main(faa_dir,names_file,id_file_faa,mergeFile_faa,eggNOG_out,clusterFolder,eggNOG_map_out)
+    try:
+        if not eggNOG_map_out.split("/")[1] in os.listdir(outputFolder):
+            names_file="allfiles.txt" #contains 50 id number of each genome
+            id_file_faa="title.txt" # contains header line in merge file that concatenate all 50 genomes faa/fna
+            mapping.main(faa_dir,names_file,id_file_faa,mergeFile_faa,eggNOG_out,clusterFolder,eggNOG_map_out)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
     """ after annotation all 50 files, then convert to gff for each annotation tool"""
+    try:
+        gff_out_folder=args.gffFolder #folder containing gff files for each annotation tool
+        if not gff_out_folder in os.listdir():
+            subprocess.call(["mkdir",gff_out_folder])
 
-    gff_out_folder=args.gffFolder #folder containing gff files for each annotation tool
-    if not gff_out_folder in os.listdir():
-        subprocess.call(["mkdir",gff_out_folder])
+        tmhmm_gff,signalP_gff,piler_gff,card_gff,door2_gff,vfdb_gff,eggnog_gff="{}/tmhmm_to_gff".format(gff_out_folder),"{}/signalP_to_gff".format(gff_out_folder)\
+            ,"{}/pilercf_to_gff".format(gff_out_folder),"{}/card_to_gff".format(gff_out_folder),\
+             "{}/door2_to_gff".format(gff_out_folder),"{}/vfdb_to_gff".format(gff_out_folder),"{}/eggnog_to_gff".format(gff_out_folder)
+       
+        toGFF3.main(tmhmm_out,tmhmm_gff,singalP_out,signalP_gff,door2_out,door2_gff,vfdb_out,vfdb_gff,card_out,card_gff,piler_out,piler_gff,eggNOG_map_out,eggnog_gff,outputFolder,gff_out_folder)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
 
-    tmhmm_gff,signalP_gff,piler_gff,card_gff,door2_gff,vfdb_gff,eggnog_gff="{}/tmhmm_to_gff".format(gff_out_folder),"{}/signalP_to_gff".format(gff_out_folder)\
-        ,"{}/pilercf_to_gff".format(gff_out_folder),"{}/card_to_gff".format(gff_out_folder),\
-         "{}/door2_to_gff".format(gff_out_folder),"{}/vfdb_to_gff".format(gff_out_folder),"{}/eggnog_to_gff".format(gff_out_folder)
-   
-    toGFF3.main(tmhmm_out,tmhmm_gff,singalP_out,signalP_gff,door2_out,door2_gff,vfdb_out,vfdb_gff,card_out,card_gff,piler_out,piler_gff,eggNOG_map_out,eggnog_gff,outputFolder,gff_out_folder)
-    
     #if there is exist all gff folders for all tool, we can begin to merge
-    all_gff_faa=args.mergeFolder
-    if all_gff_faa not in os.listdir():
-        mergeAll_to_gff.main(tmhmm_gff,signalP_gff,piler_gff,card_gff,door2_gff,vfdb_gff,eggnog_gff,all_gff_faa)
+    try:
+        all_gff_faa=args.mergeFolder
+        if all_gff_faa not in os.listdir():
+            mergeAll_to_gff.main(tmhmm_gff,signalP_gff,piler_gff,card_gff,door2_gff,vfdb_gff,eggnog_gff,all_gff_faa)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
 if __name__ == "__main__":
     main()
